2c530af6a7b152827e7356f0cdba0e17f4861197
PHOENIXIT\vshigorova <vshigorova@fil-it.ru>
2023-05-20 14:26:26 +0300

    command pre/postconditions

--- src/test/java/lesson5/jUnitTest.java
+++ src/test/java/lesson5/jUnitTest.java
@@ -1,2 +1,45 @@
-package Lesson5;public class jUnitTest {
+package lesson5;
+
+import org.junit.jupiter.api.*;
+
+public class jUnitTest {
+
+    @BeforeAll
+    static void beforeAll(){
+
+        System.out.println("Это сметод beforeAll");
+    }
+
+    @AfterAll
+    static void afterAll(){
+
+        System.out.println("Это сметод afterAll");
+    }
+
+
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("Это метод @BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("Это метод @AfterEach");
+    }
+
+
+    @Test
+    void firstTest(){
+        System.out.println("Это тест");
+        Assertions.assertTrue(3>2);
+
+    }
+
+    @Test
+    void secondTest(){
+        System.out.println("Это secondTest");
+        Assertions.assertTrue(3>2);
+
+    }
 }
